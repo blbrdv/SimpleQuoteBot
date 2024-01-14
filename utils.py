271420a33ffa600ab75141009bddd88e323432e1
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-01-14 22:47:00 +0700

    fix text size using

--- utils.py
+++ utils.py
@@ -9,13 +9,13 @@ def _break_fix(text, width, font, draw):
     while lo < hi:
         mid = (lo + hi + 1) // 2
         t = text[:mid]
-        w, h = draw.textsize(t, font=font)
+        w, h = text_size(t, font=font)
         if w <= width:
             lo = mid
         else:
             hi = mid - 1
     t = text[:lo]
-    w, h = draw.textsize(t, font=font)
+    w, h = text_size(t, font=font)
     yield t, w, h
     yield from _break_fix(text[lo:], width, font, draw)
 
