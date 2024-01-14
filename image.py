e91e5e659f3b433d6ec486284e88f9d6dd9c545b
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-01-14 16:34:28 +0700

    add background drawing

--- image.py
+++ image.py
@@ -2,11 +2,16 @@ from PIL import Image, ImageDraw
 
 
 def draw(name: str):
-    image = Image.new("RGBA", (400, 400), (0, 0, 0, 0))
-    d = ImageDraw.Draw(image)
+    bg_raw = Image.open("bg.png").convert("RGBA")
+    bg = bg_raw.resize((400, 400))
+    image = Image.new("RGBA", (400, 400), (0, 0, 0))
+    canvas = image.copy()
 
+    canvas.paste(bg)
+
+    d = ImageDraw.Draw(image)
     d.rounded_rectangle(
         (10, 10, 50, 50), radius=5, fill="#202123", corners=(True, True, True, False)
     )
 
-    image.save(name)
+    canvas.save(name)
