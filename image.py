985d09cc3b20e92c4932f47dd74a029d1b9c2801
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-01-15 21:13:51 +0700

    add message block drawing

--- image.py
+++ image.py
@@ -1,29 +1,61 @@
 from PIL import Image, ImageDraw, ImageFont
 
-from utils import text_size, fit_text
+from utils import text_size
 
 
-def draw(messages: list[str], name: str):
-    bg_raw = Image.open("bg.png").convert("RGBA")
-    bg = bg_raw.resize((512, 512))
-    image = Image.new("RGBA", (512, 512), (0, 0, 0))
-    canvas = image.copy()
+TEXT_FONT = ImageFont.truetype("font.ttf", 14)
+MAX_CANVAS_WIDTH = 512
+PADDING = 10
+TOTAL_PADDING = PADDING * 2
 
-    canvas.paste(bg)
 
-    font = ImageFont.truetype("font.ttf", 14)
-    d = ImageDraw.Draw(canvas)
+def draw(messages: list[str], name: str) -> None:
+    canvas_height = TOTAL_PADDING
+    canvas_width = 0
 
     for message in messages:
-        (w, _) = text_size(message, font)
+        (width, height) = _message_size(message)
 
-        d.rounded_rectangle(
-            (10, 10, 492, 50), radius=5, fill="#202123", corners=(True, True, True, False)
-        )
+        canvas_height += height
+        if width > canvas_width:
+            canvas_width = width
 
-        if w > 472:
-            fit_text(canvas, message, "white", font)
-        else:
-            d.text((20, 20), message, "white", font)
+    bg_raw = Image.open("bg.png").convert("RGBA")
+    bg = bg_raw.resize((canvas_width + TOTAL_PADDING, canvas_height + TOTAL_PADDING))
+    canvas = Image.new(
+        "RGBA", (canvas_width + TOTAL_PADDING, canvas_height + TOTAL_PADDING), (0, 0, 0)
+    )
+    canvas.paste(bg)
+
+    message_image_y = PADDING
+    for message in messages:
+        message_image = _draw_message(message, (canvas_width + TOTAL_PADDING, canvas_height + TOTAL_PADDING), message_image_y)
+
+        canvas = Image.alpha_composite(canvas, message_image)
+
+        (_, message_image_height) = _message_size(message)
+        message_image_y += message_image_height + PADDING
 
     canvas.save(name)
+
+
+def _message_size(text: str) -> tuple[int, int]:
+    (text_width, text_height) = text_size(text, TEXT_FONT)
+
+    return text_width + TOTAL_PADDING + PADDING, text_height + TOTAL_PADDING
+
+
+def _draw_message(text: str, size: tuple[int, int], y: int) -> Image:
+    (canvas_width, canvas_height) = _message_size(text)
+    canvas = Image.new("RGBA", (size[0], size[1]), (0, 0, 0, 0))
+
+    d = ImageDraw.Draw(canvas)
+    d.rounded_rectangle(
+        (PADDING, y, canvas_width, y + canvas_height),
+        radius=5,
+        fill="#202123",
+        corners=(True, True, True, False),
+    )
+    d.text((TOTAL_PADDING, y + PADDING), text, fill="white", font=TEXT_FONT)
+
+    return canvas
