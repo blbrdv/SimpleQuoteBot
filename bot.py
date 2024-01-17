e5e51f18f30c86444c7f67694b921e682e141ae7
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-01-17 17:55:21 +0700

    move to aiogram

--- bot.py
+++ bot.py
@@ -1,20 +1,24 @@
+import asyncio
+import logging
 import os
 import traceback
 import sys
-from datetime import datetime
+from datetime import timezone
 from os import getenv
 
-from pyrogram import Client, filters
-from pyrogram.types import Message
+from aiogram import Dispatcher, Bot, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import BotCommand, Message, InputFile, FSInputFile
 
 from image import draw
 
-app = Client("QuoteBot", bot_token=getenv("BOT_TOKEN"))
+dispatcher = Dispatcher()
 history = {}
 
 
-@app.on_message(filters.command(["start"]))
-async def _on_start(_, message: Message) -> None:
+@dispatcher.message(CommandStart())
+async def _on_start(message: Message) -> None:
     await message.reply(
         "1. Forward messages to private chat with bot.\n"
         "2. Reply '/quote' command on first message.\n"
@@ -23,22 +27,23 @@ async def _on_start(_, message: Message) -> None:
     )
 
 
-@app.on_message(filters.command(["quote"]))
-async def _on_quote(client, message: Message) -> None:
-    if not message.reply_to_message:
-        await _on_start(client, message)
+@dispatcher.message(Command(BotCommand(command="quote", description="Create quote")))
+async def _on_quote(message: Message) -> None:
+    reply = message.reply_to_message # storing it b/c aiogram is retarded
+    if not reply:
+        await _on_start(message)
         return
 
     # not supposed to happen
     if not history:
-        await _on_start(client, message)
+        await _on_start(message)
         return
 
     messages = []
-    for key, (text, date) in history[message.chat.id].items():
-        if key >= message.reply_to_message_id:
-            time = date.astimezone(datetime.UTC).strftime("%H:%M")
-            messages.append((text, time))
+    for key, message in history[message.chat.id].items():
+        if key >= reply.message_id:
+            time = message.date.replace(tzinfo=timezone.utc).strftime("%H:%M")
+            messages.append((message.md_text, time))
 
     # not supposed to happen
     if not messages:
@@ -49,7 +54,7 @@ async def _on_quote(client, message: Message) -> None:
 
     try:
         draw(messages, file_name)
-        await message.reply_photo(file_name)
+        await message.reply_photo(FSInputFile(file_name))
 
         os.remove(file_name)
         history[message.chat.id].clear()
@@ -60,8 +65,8 @@ async def _on_quote(client, message: Message) -> None:
             print(traceback.format_exc(), file=sys.stderr)
 
 
-@app.on_message()
-async def _on_message(_, message: Message) -> None:
+@dispatcher.message()
+async def _on_message(message: types.Message) -> None:
     try:
         _ = history[message.chat.id]
     except:
@@ -73,8 +78,16 @@ async def _on_message(_, message: Message) -> None:
         await message.reply("Too big request. Try to split your messages.")
         history[message.chat.id] = {}
     else:
-        history[message.chat.id][message.id] = message
+        history[message.chat.id][message.message_id] = message
+
+
+async def _start_bot() -> None:
+    bot = Bot(getenv("BOT_TOKEN"), parse_mode=ParseMode.MARKDOWN)
+
+    print('(Press Ctrl+C to stop this)')
+    await dispatcher.start_polling(bot)
 
 
 def main() -> None:
-    app.run()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(_start_bot())
