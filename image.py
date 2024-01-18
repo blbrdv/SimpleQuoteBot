32df98ddc999749b1d431469d0a79a7a26781156
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-01-18 13:30:29 +0700

    add size dataclass

--- image.py
+++ image.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 from Message import Message
+from Size import Size
 from Speech import Speech
 
 TEXT_FONT = ImageFont.truetype("font.ttf", 14)
@@ -14,10 +15,10 @@ def draw(data: list[Speech], name: str) -> None:
     canvas_width = 50
 
     for speech in data:
-        (width, height) = _speech_size(speech)
+        speech_size = _speech_size(speech)
 
-        canvas_height += height + TOTAL_MARGIN
-        full_width = width + TOTAL_MARGIN + 50
+        canvas_height += speech_size.height + TOTAL_MARGIN
+        full_width = speech_size.width + TOTAL_MARGIN + 50
         if full_width > canvas_width:
             canvas_width = full_width
 
@@ -29,38 +30,35 @@ def draw(data: list[Speech], name: str) -> None:
     speech_image_y = 0
     for speech in data:
         speech_image = _draw_speech(
-            speech, (canvas_width, canvas_height), speech_image_y
+            speech, Size(canvas_width, canvas_height), speech_image_y
         )
 
         canvas = Image.alpha_composite(canvas, speech_image)
 
-        (_, speech_image_height) = _speech_size(speech)
         speech_image_y += speech_image_y
 
     canvas.save(name)
 
 
 def _draw_speech(
-    speech: Speech, canvas_size: tuple[int, int], speech_image_y: int
+    speech: Speech, canvas_size: Size, speech_image_y: int
 ) -> Image:
-    _, height = _speech_size(speech)
-
-    canvas = Image.new("RGBA", (canvas_size[0], canvas_size[1]), (0, 0, 0, 0))
+    canvas = Image.new("RGBA", (canvas_size.width, canvas_size.height), (0, 0, 0, 0))
 
     count = 1
     message_image_y = TOTAL_MARGIN + speech_image_y
     for message in speech.messages:
         message_image = _draw_message(
             message,
-            (canvas_size[0], canvas_size[1]),
+            canvas_size,
             message_image_y,
             count == len(speech.messages),
         )
 
         canvas = Image.alpha_composite(canvas, message_image)
 
-        (_, message_image_height) = _message_size(message)
-        message_image_y += message_image_height + TOTAL_MARGIN
+        message_size = _message_size(message)
+        message_image_y += message_size.height + TOTAL_MARGIN
 
         count += 1
 
@@ -68,17 +66,16 @@ def _draw_speech(
 
 
 def _draw_message(
-    message: Message, canvas_size: tuple[int, int], y: int, last: bool = False
+    message: Message, canvas_size: Size, y: int, last: bool = False
 ) -> Image:
-    (block_width, block_height) = _message_size(message)
-    (text_width, _) = _text_size(message.text, TEXT_FONT)
+    message_size = _message_size(message)
 
-    canvas = Image.new("RGBA", (canvas_size[0], canvas_size[1]), (0, 0, 0, 0))
+    canvas = Image.new("RGBA", (canvas_size.width, canvas_size.height), (0, 0, 0, 0))
 
     d = ImageDraw.Draw(canvas)
 
     d.rounded_rectangle(
-        (MARGIN, y, block_width, y + block_height),
+        (MARGIN, y, message_size.width, y + message_size.height),
         radius=5,
         fill="#202123",
         corners=(True, True, True, not last),
@@ -91,35 +88,35 @@ def _draw_message(
     return canvas
 
 
-def _speech_size(speech: Speech) -> tuple[int, int]:
+def _speech_size(speech: Speech) -> Size:
     canvas_height = 0
     canvas_width = 0
 
     for message in speech.messages:
-        (width, height) = _message_size(message)
+        message_size = _message_size(message)
 
-        canvas_height += height + TOTAL_MARGIN
-        if width + TOTAL_MARGIN > canvas_width:
-            canvas_width = width + TOTAL_MARGIN
+        canvas_height += message_size.height + TOTAL_MARGIN
+        if message_size.width + TOTAL_MARGIN > canvas_width:
+            canvas_width = message_size.width + TOTAL_MARGIN
 
-    return canvas_width, canvas_height
+    return Size(canvas_width, canvas_height)
 
 
-def _message_size(message: Message) -> tuple[int, int]:
-    (text_width, text_height) = _text_size(message.text, TEXT_FONT)
+def _message_size(message: Message) -> Size:
+    text_size = _text_size(message.text, TEXT_FONT)
 
-    return (
-        text_width + TOTAL_MARGIN + TOTAL_MARGIN,
-        text_height + TOTAL_MARGIN,
+    return Size(
+        text_size.width + TOTAL_MARGIN + TOTAL_MARGIN,
+        text_size.height + TOTAL_MARGIN,
     )
 
 
 # https://stackoverflow.com/a/77749307/23112474
-def _text_size(text, font):
+def _text_size(text, font) -> Size:
     image = Image.new(mode="P", size=(0, 0))
     d = ImageDraw.Draw(image)
     _, _, width, height = d.textbbox((0, 0), text=text, font=font)
-    return width, height
+    return Size(width, height)
 
 
 # def _break_fix(text, width, font, draw):
