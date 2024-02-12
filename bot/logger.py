3c3832a24d5b4b5991cd13c052ae9c8a9a5a502f
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-02-12 20:28:25 +0700

    fix writing to stdout

--- bot/logger.py
+++ bot/logger.py
@@ -1,48 +1,37 @@
 import logging
 import sys
+from logging.handlers import TimedRotatingFileHandler
 from os import getenv
-
-
-# https://stackoverflow.com/a/31688396/23112474
-class LoggerWriter:
-    def __init__(self, level):
-        # self.level is really like using log.debug(message)
-        # at least in my case
-        self.level = level
-
-    def write(self, message):
-        # if statement reduces the amount of newlines that are
-        # printed to the logger
-        if message != "\n":
-            self.level(message)
-
-    def flush(self):
-        # create a flush method so things can be flushed when
-        # the system wants to. Not sure if simply 'printing'
-        # sys.stderr is the correct way to do it, but it seemed
-        # to work properly for me.
-        self.level(sys.stderr)
 
 
 class Logger:
     def __init__(self, name: str):
-        self.log = logging.getLogger(name)
+        self.logger = logging.getLogger(name)
         if getenv("DEBUG"):
-            logging.basicConfig(filename=".log", encoding="utf-8", level=logging.DEBUG)
-            sys.stdout = LoggerWriter(self.log.debug)
+            self.logger.setLevel(logging.DEBUG)
         else:
-            logging.basicConfig(filename=".log", encoding="utf-8", level=logging.INFO)
-            sys.stdout = LoggerWriter(self.log.info)
-        sys.stderr = LoggerWriter(self.log.warning)
+            self.logger.setLevel(logging.INFO)
+
+        formater = logging.Formatter("%(name)-13s: %(levelname)-8s %(message)s")
+
+        file_handler = TimedRotatingFileHandler(
+            ".log", when="d", interval=1, backupCount=30
+        )
+        file_handler.setFormatter(formater)
+        self.logger.addHandler(file_handler)
+
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formater)
+        self.logger.addHandler(stream_handler)
 
     def debug(self, message: str):
-        self.log.debug(message)
+        self.logger.debug(message)
 
     def info(self, message: str):
-        self.log.info(message)
+        self.logger.info(message)
 
     def warning(self, message: str):
-        self.log.warning(message)
+        self.logger.warning(message)
 
     def error(self, message: str):
-        self.log.error(message)
+        self.logger.error(message)
