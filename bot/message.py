a6b4f88383243fee3c211d594682304762e2910a
blbrdv <95272917+blbrdv@users.noreply.github.com>
2024-05-11 15:55:29 +0700

    fixed chat user

--- bot/message.py
+++ bot/message.py
@@ -145,6 +145,14 @@ class Bubble(IncomingMessage):
                 if message.forward_origin.type == MessageOriginType.CHANNEL:
                     user_id = message.forward_origin.chat.id
                     first_name = message.forward_origin.chat.full_name
+                elif message.forward_origin.type == MessageOriginType.CHAT:
+                    user_id = message.forward_from_chat.id
+                    name = message.forward_from_chat.full_name.split()
+                    first_name = name[0]
+                    if name[1:] is not None and name[1:] != "":
+                        last_name = " ".join(name[1:])
+
+                    # TODO: chat pfp
                 else:
                     user_id = message.forward_origin.sender_user.id
                     first_name = message.forward_origin.sender_user.first_name
